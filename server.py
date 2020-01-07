1e68c07d6c3525ec872886e5af80e6548b8a0614
kazzastic <rkazim79@gmail.com>
2020-01-07 18:31:07 +0500

    making a 3 tier arch

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 classes = ["normal", "cancer"]
 
-
+'''
 def prepare(filepath): 
     img_array = cv2.imread(filepath)
     new_array = cv2.resize(img_array, (height, width))
@@ -26,21 +26,26 @@ def prepare(filepath):
     return new_array.reshape(-1, height, width, 3)
 
 
-model = tf.keras.models.load_model("ddsm_vgg16_s10_[512-512-1024]x2_hybrid.h5")
+model = tf.keras.models.load_model("model/ddsm_vgg16_s10_[512-512-1024]x2_hybrid.h5")
 height = model.input.shape[1]
 width = model.input.shape[2]
 
-prediction = model.predict([prepare('cancer.jpg')])
+prediction = model.predict([prepare('model/cancer.jpg')])
 print(prediction)  # will be a list in a list.
 normal = math.ceil(prediction[0][1]*100)
 cancer = math.ceil(prediction[0][0]*100)
 result = [{'id': 0, 'title':'Normal', 'Prediction': normal},
           {'id': 1, 'title':'Cancer', 'Prediction': cancer}]
 print("normal {0}\ncancer {1}".format(normal, cancer))
+'''
 
-@app.route('/')
+
+@app.route('/', methods = ['GET', 'POST'])
 def index_api_call():
+    model  = tf.keras.models.load_model("model/ddsm_vgg16_s10_[512-512-1024]x2_hybrid.h5")
+    image_url = request.files['']
     return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',port = '8000')
